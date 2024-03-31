46f7dea55f3d9b1879a531cd36e69455ef0fb78a
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-03-31 18:50:16 -0300

    feat: add post links api

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,40 @@
 import fastify from "fastify"
+import { z } from "zod"
+import { sql } from "./lib/postgres"
+import postgres from "postgres"
 
 const app = fastify()
 
 app.get("/test", () => {
   return { message: "Hello World" }
+})
+app.post("/api/links", async (request, reply) => {
+  const createLinkSchema = z.object({
+    code: z.string().min(3),
+    url: z.string().url(),
+  })
+
+  const { code, url } = createLinkSchema.parse(request.body)
+
+  try {
+    const result = await sql/*sql*/ `
+  INSERT INTO short_links (code, original_url)
+  VALUES (${code}, ${url})
+  RETURNING id
+  `
+
+    const link = result[0]
+
+    return reply.status(201).send({ shortLinkId: link.id })
+  } catch (error) {
+    if (error instanceof postgres.PostgresError) {
+      if (error.code === "23505") {
+        return reply.status(400).send({ error: "Code already in use" })
+      }
+    }
+
+    console.log(error)
+  }
 })
 
 app
