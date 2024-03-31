d4e059ca7093737ba44208b1882a4e1480aa5379
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-03-31 13:08:21 -0300

    test husky + list-staged

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # short-url-node
- 
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import fastify from 'fastify';
+import fastify from "fastify"
 
-const app = fastify();
+const app = fastify()
 
-app.get('/test', () => {
-  return { message: 'Hello World' };
+app.get("/test", () => {
+  return { message: "Hello World" }
 })
 
-app.listen({
-  port: 3000,
-}).then(() => {
-  console.log('Server is running on port 3000');
-})
+app
+  .listen({
+    port: 3000,
+  })
+  .then(() => {
+    console.log("Server is running on port 3000")
+  })
