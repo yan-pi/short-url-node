3f935b9958c76c0e661f1b9ad6e1cdd3a5049e37
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-03-31 23:14:46 -0300

    feat: add redis ranking

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify"
 import { z } from "zod"
 import { sql } from "./lib/postgres"
+import { redis } from "./lib/redis"
 import postgres from "postgres"
 
 const app = fastify()
@@ -21,6 +22,8 @@ app.get("/:code", async (request, reply) => {
   }
 
   const link = result[0]
+
+  await redis.zIncrBy("metrics", 1, String(link.id))
 
   return reply.redirect(301, link.original_url)
 })
@@ -43,10 +46,10 @@ app.post("/api/links", async (request, reply) => {
 
   try {
     const result = await sql/*sql*/ `
-  INSERT INTO short_links (code, original_url)
-  VALUES (${code}, ${url})
-  RETURNING id
-  `
+    INSERT INTO short_links (code, original_url)
+    VALUES (${code}, ${url})
+    RETURNING id
+    `
 
     const link = result[0]
 
@@ -57,8 +60,27 @@ app.post("/api/links", async (request, reply) => {
         return reply.status(400).send({ error: "Code already in use" })
       }
     }
-
     console.log(error)
+    throw new Error("Unexpected error")
+  }
+})
+
+app.get("/api/metrics", async () => {
+  try {
+    const result = await redis.zRangeByScoreWithScores("metrics", 0, 50)
+    const metrics = result.sort((a, b) => a.score - b.score)
+    .map(item => {
+      return {
+        shortLinkId: Number(item.value),
+        clicks: item.score,
+      }
+    })
+  
+    return metrics
+    
+  } catch (error) {
+    console.error("Error fetching metrics:", error)
+    return { error: "Failed to fetch metrics" }
   }
 })
 
