f85633b0c10211607e1da023cfe5e429ca07bb9a
Yan Fernandes <102800044+yan-pi@users.noreply.github.com>
2024-04-01 23:42:40 -0300

    fix: fix redis on listMetrics

--- src/routes/listMetricsRoute.ts
+++ src/routes/listMetricsRoute.ts
@@ -5,14 +5,21 @@ export default async function listMetricsRoute(fastify: FastifyInstance) {
   fastify.get('/api/metrics', async () => {
     try {
       const result = await redis.zrangebyscore('metrics', 0, 50, 'WITHSCORES');
-      const metrics = result
-        .sort((a, b) => Number(a[1]) - Number(b[1]))
-        .map((item) => ({
-          shortLinkId: Number(item[0]),
-          clicks: Number(item[1]),
-        }));
+      const metrics = result.reduce((acc: { [key: string]: number }, item, index, array) => {
+        if (index % 2 === 0) {
+          const shortLinkId = Number(item);
+          const clicks = Number(array[index + 1]);
+          acc[shortLinkId] = (acc[shortLinkId] || 0) + clicks;
+        }
+        return acc;
+      }, {} as { [key: string]: number });
 
-      return metrics;
+      const formattedMetrics = Object.keys(metrics).map((shortLinkId) => ({
+        shortLinkId: Number(shortLinkId),
+        clicks: metrics[shortLinkId],
+      }));
+
+      return formattedMetrics;
     } catch (error) {
       console.error('Error fetching metrics:', error);
       return { error: 'Failed to fetch metrics' };
